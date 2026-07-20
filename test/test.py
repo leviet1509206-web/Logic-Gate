98e760329a62e5ed4a5d0549e24583ea2d7e77d7
leviet1509206-web <leviet1509206@gmail.com>
2026-07-20 17:27:35 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,34 +7,46 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_full_adder(dut):
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
     dut.uio_in.value = 0
+
+    # Reset
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 5)
+
     dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    truth_table = [
+        (0,0,0,0,0),
+        (0,0,1,1,0),
+        (0,1,0,1,0),
+        (0,1,1,0,1),
+        (1,0,0,1,0),
+        (1,0,1,0,1),
+        (1,1,0,0,1),
+        (1,1,1,1,1),
+    ]
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    for A,B,Cin,Sum,Cout in truth_table:
+
+        dut.ui_in.value = (Cin<<2) | (B<<1) | A
+
+        await ClockCycles(dut.clk,1)
+
+        actual_sum = int(dut.uo_out.value) & 1
+        actual_cout = (int(dut.uo_out.value)>>1)&1
+
+        dut._log.info(
+            f"A={A} B={B} Cin={Cin} -> "
+            f"Sum={actual_sum} Cout={actual_cout}"
+        )
+
+        assert actual_sum == Sum
+        assert actual_cout == Cout
